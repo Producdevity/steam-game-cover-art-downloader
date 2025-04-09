87bec7ab20af059232ad6cc2bc7ddc65693c5894
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-04-10 00:03:10 +0200

    feat: update updateGamesList.sh to use curl for fetching game list data

--- scripts/updateGamesList.sh
+++ scripts/updateGamesList.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O src/db/steamAppList2.json "https://api.steampowered.com/ISteamApps/GetAppList/V2/?format=json"
+curl -s "https://api.steampowered.com/ISteamApps/GetAppList/V2/?format=json" -o ./src/db/steamAppList.json
